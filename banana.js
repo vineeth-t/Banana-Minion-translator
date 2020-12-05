2c234c950a207a58adc0abf2c8090f47392a7139
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2020-12-05 17:13:46 +0530

    feat:added translation

--- banana.js
+++ banana.js
@@ -1,2 +1,30 @@
-var inputDiv=document.querySelector("btn-translate");
-console.log(inputDiv);
+var btnTranslate=document.querySelector("#btn-translate");
+var outputDiv = document.querySelector("#output");
+var textInput= document.querySelector(".textArea");
+var url="https://api.funtranslations.com/translate/minion.json";
+btnTranslate.addEventListener("click",clickEvent);
+function clickEvent() {
+    doFetch(textInput.value);
+    
+}
+
+
+function constructUrl(text){
+    return url+"?"+"text="+text;
+}
+function doFetch(text){
+    fetch(constructUrl(text))
+        .then(response => response.json())
+        .then(json => console.log(outputDiv.innerText=json.contents.translated))
+        .catch(errorHandler)
+}
+
+/* error Handling*/
+function errorHandler(error){
+        console.log(('error occurred',error));
+        alert("Please try again later");
+}
+
+
+
+
